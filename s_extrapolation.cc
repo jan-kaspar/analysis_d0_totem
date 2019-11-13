404e0e9bc88a95af971036e9c5fef4ed306fd60b
Jan Kašpar <jan.kaspar@cern.ch>
2019-11-13 19:11:41 +0100

    Calculates also uncertainty from t-values.

--- s_extrapolation.cc
+++ s_extrapolation.cc
@@ -1,4 +1,5 @@
 #include "TFile.h"
+#include "TH1D.h"
 #include "TGraphErrors.h"
 #include "TF1.h"
 #include "TMatrixDSym.h"
@@ -19,6 +20,13 @@ using namespace std;
 //----------------------------------------------------------------------------------------------------
 
 int n_ff_parameters;
+
+const unsigned int n_dsdt_samples = 200;
+
+// TODO: rename
+const unsigned int n_repetitions = 1000;
+
+string fitModel = "";
 
 //----------------------------------------------------------------------------------------------------
 
@@ -182,7 +190,6 @@ void SaveFitPlots(const ROOT::Fit::FitResult &result, const string &extrapolatio
 	// sample fit uncertainty
 	vector<Stat> st(vg_fit[0]->GetN(), Stat(n_ff_parameters));
 
-	const unsigned int n_repetitions = 100;
 	for (unsigned int ir = 0; ir < n_repetitions; ++ir)
 	{
 		// generate biased extrapolation
@@ -209,6 +216,7 @@ void SaveFitPlots(const ROOT::Fit::FitResult &result, const string &extrapolatio
 		}
 	}
 
+	// build plots
 	for (int i = 0; i < n_ff_parameters; ++i)
 	{
 		for (int iw = 0; iw < vg_fit[i]->GetN(); ++iw)
@@ -244,46 +252,9 @@ void SaveFitPlots(const ROOT::Fit::FitResult &result, const string &extrapolatio
 
 //----------------------------------------------------------------------------------------------------
 
-const unsigned int n_points = 200;
-
-void SaveExtrapolation(const ROOT::Fit::FitResult &result, const string &extrapolationModel, const Dataset &ds)
+void SaveParameterUncertaintyBand(const ROOT::Fit::FitResult &result, const string &extrapolationModel, const Dataset &ds,
+		const double *par, const double ep, const TGraph *g_ref, TGraph *g_dsdt_ext_unc, TGraph *g_B_ext_unc)
 {
-	// calculate extrapolated parameters
-	const double *par = result.GetParams();
-
-	TVectorD a(n_ff_parameters), b(n_ff_parameters);
-	for (int i = 0; i < n_ff_parameters; ++i)
-	{
-		a(i) = par[i];
-		b(i) = par[n_ff_parameters + i];
-	}
-
-	TVectorD p = EvaluateParameters(a, b, ds.sqrt_s, extrapolationModel);
-
-	// set extrapolated parameters to fit function
-	for (int i = 0; i < n_ff_parameters; ++i)
-		ds.ff->SetParameter(i, p(i));
-
-	// evaluate extrapolated graph
-	TGraph *g_dsdt_ext = new TGraph(); g_dsdt_ext->SetName("g_dsdt_ext");
-	TGraph *g_B_ext = new TGraph(); g_B_ext->SetName("g_B_ext");
-
-	const double ep = 1E-4;
-
-	for (unsigned int i = 0; i < n_points; ++i)
-	{
-		const double t = ds.t_min + (ds.t_max - ds.t_min) / (n_points - 1) * i;
-		const double f_dsdt = ds.ff->Eval(t);
-		const double f_B = (log(ds.ff->Eval(t + ep)) - log(f_dsdt)) / ep;
-
-		int idx = g_dsdt_ext->GetN();
-		g_dsdt_ext->SetPoint(idx, t, f_dsdt);
-		g_B_ext->SetPoint(idx, t, f_B);
-	}
-
-	g_dsdt_ext->Write();
-	g_B_ext->Write();
-
 	// prepare pertubation generator matrix
 	int dim = result.NPar();
 	TMatrixDSym V(dim);
@@ -299,8 +270,17 @@ void SaveExtrapolation(const ROOT::Fit::FitResult &result, const string &extrapo
 	TMatrixD m_gen = eig_decomp.GetEigenVectors() * S;
 
 	// uncertainty band
-	const unsigned int n_repetitions = 100;
-	vector<Stat> stat(n_points, Stat(1)), stat_B(n_points, Stat(1));
+	vector<Stat> stat(n_dsdt_samples, Stat(1)), stat_B(n_dsdt_samples, Stat(1));
+
+	TH1D *h_dsdt_mod_at_t_min = new TH1D("", "dsdt_mod(t_min)", 100., 0., 0.);
+	TH1D *h_dsdt_mod_at_t_dip = new TH1D("", "dsdt_mod(t_dip)", 100., 0., 0.);
+	TH1D *h_dsdt_mod_at_t_bmp = new TH1D("", "dsdt_mod(t_bmp)", 100., 0., 0.);
+	TH1D *h_dsdt_mod_at_t_max = new TH1D("", "dsdt_mod(t_max)", 100., 0., 0.);
+
+	const double dsdt_at_t_min = g_ref->Eval(ds.t_min);
+	const double dsdt_at_t_dip = g_ref->Eval(ds.t_dip);
+	const double dsdt_at_t_bmp = g_ref->Eval(ds.t_bmp);
+	const double dsdt_at_t_max = g_ref->Eval(ds.t_max);
 
 	for (unsigned int ir = 0; ir < n_repetitions; ++ir)
 	{
@@ -325,10 +305,31 @@ void SaveExtrapolation(const ROOT::Fit::FitResult &result, const string &extrapo
 		for (int i = 0; i < n_ff_parameters; ++i)
 			ff_mod->SetParameter(i, p_mod(i));
 
+		// acceptable configuration ??
+		const double dsdt_mod_at_t_min = ff_mod->Eval(ds.t_min);
+		const double dsdt_mod_at_t_dip = ff_mod->Eval(ds.t_dip);
+		const double dsdt_mod_at_t_bmp = ff_mod->Eval(ds.t_bmp);
+		const double dsdt_mod_at_t_max = ff_mod->Eval(ds.t_max);
+
+		bool accept = (
+				fabs(dsdt_mod_at_t_min - dsdt_at_t_min) / dsdt_at_t_min < 2.0
+				&& fabs(dsdt_mod_at_t_dip - dsdt_at_t_dip) / dsdt_at_t_dip < 2.0
+				&& fabs(dsdt_mod_at_t_bmp - dsdt_at_t_bmp) / dsdt_at_t_bmp < 2.0
+				&& fabs(dsdt_mod_at_t_max - dsdt_at_t_max) / dsdt_at_t_max < 2.0
+			);
+
+		if (!accept)
+			continue;
+
+		h_dsdt_mod_at_t_min->Fill(dsdt_mod_at_t_min);
+		h_dsdt_mod_at_t_dip->Fill(dsdt_mod_at_t_dip);
+		h_dsdt_mod_at_t_bmp->Fill(dsdt_mod_at_t_bmp);
+		h_dsdt_mod_at_t_max->Fill(dsdt_mod_at_t_max);
+
 		// sample biased extrapolation
-		for (int i = 0; i < g_dsdt_ext->GetN(); ++i)
+		for (int i = 0; i < g_ref->GetN(); ++i)
 		{
-			const double t = g_dsdt_ext->GetX()[i];
+			const double t = g_ref->GetX()[i];
 			const double f_dsdt_mod = ff_mod->Eval(t);
 			const double f_B_mod = (log(ff_mod->Eval(t + ep)) - log(f_dsdt_mod)) / ep;
 
@@ -340,38 +341,213 @@ void SaveExtrapolation(const ROOT::Fit::FitResult &result, const string &extrapo
 		delete ff_mod;
 	}
 
+	// write debug plots
+	h_dsdt_mod_at_t_min->Write("h_dsdt_mod_at_t_min");
+	h_dsdt_mod_at_t_dip->Write("h_dsdt_mod_at_t_dip");
+	h_dsdt_mod_at_t_bmp->Write("h_dsdt_mod_at_t_bmp");
+	h_dsdt_mod_at_t_max->Write("h_dsdt_mod_at_t_max");
+
 	// build graphs
-	TGraph *g_dsdt_ext_unc = new TGraph();
-	TGraph *g_dsdt_ext_pl_unc = new TGraph();
-	TGraph *g_dsdt_ext_mi_unc = new TGraph();
-
-	TGraph *g_B_ext_pl_unc = new TGraph();
-	TGraph *g_B_ext_mi_unc = new TGraph();
-
-	for (int i = 0; i < g_dsdt_ext->GetN(); ++i)
+	for (int i = 0; i < g_ref->GetN(); ++i)
 	{
-		const double t = g_dsdt_ext->GetX()[i];
-		const double f_dsdt = g_dsdt_ext->GetY()[i];
+		const double t = g_ref->GetX()[i];
+
 		const double f_dsdt_unc = stat[i].GetStdDev(0);
+		g_dsdt_ext_unc->SetPoint(i, t, f_dsdt_unc);
 
-		const double f_B = g_B_ext->GetY()[i];
 		const double f_B_unc = stat_B[i].GetStdDev(0);
+		g_B_ext_unc->SetPoint(i, t, f_B_unc);
+	}
+}
 
-		int idx = g_dsdt_ext_unc->GetN();
-		g_dsdt_ext_unc->SetPoint(idx, t, f_dsdt_unc);
-		g_dsdt_ext_pl_unc->SetPoint(idx, t, f_dsdt + f_dsdt_unc);
-		g_dsdt_ext_mi_unc->SetPoint(idx, t, f_dsdt - f_dsdt_unc);
+//----------------------------------------------------------------------------------------------------
 
-		g_B_ext_pl_unc->SetPoint(idx, t, f_B + f_B_unc);
-		g_B_ext_mi_unc->SetPoint(idx, t, f_B - f_B_unc);
+void SaveTValueUncertaintyBand(const ROOT::Fit::FitResult &result, const string &extrapolationModel, const Dataset &ds,
+		const double *par, const double ep, const TGraph *g_ref, TGraph *g_dsdt_ext_unc, TGraph *g_B_ext_unc)
+{
+	// prepare parameter values
+	TVectorD a(n_ff_parameters), b(n_ff_parameters);
+	for (int i = 0; i < n_ff_parameters; ++i)
+	{
+		a(i) = par[i];
+		b(i) = par[n_ff_parameters + i];
 	}
 
-	g_dsdt_ext_unc->Write("g_dsdt_ext_unc");
-	g_dsdt_ext_pl_unc->Write("g_dsdt_ext_pl_unc");
-	g_dsdt_ext_mi_unc->Write("g_dsdt_ext_mi_unc");
+	TVectorD p = EvaluateParameters(a, b, ds.sqrt_s, extrapolationModel);
 
-	g_B_ext_pl_unc->Write("g_B_ext_pl_unc");
-	g_B_ext_mi_unc->Write("g_B_ext_mi_unc");
+	// prepare pertubation generator matrix
+	TMatrixDSymEigen eig_decomp(ds.m_t_value_cov);
+	TVectorD eig_values(eig_decomp.GetEigenValues());
+	TMatrixDSym S(ds.m_t_value_cov.GetNrows());
+	for (int i = 0; i < ds.m_t_value_cov.GetNrows(); i++)
+		S(i, i) = (eig_values(i) >= 0.) ? sqrt(eig_values(i)) : 0.;
+	TMatrixD m_gen = eig_decomp.GetEigenVectors() * S;
+
+	// uncertainty band
+	vector<Stat> stat(n_dsdt_samples, Stat(1)), stat_B(n_dsdt_samples, Stat(1));
+
+	TH1D *h_t_min = new TH1D("", ";t_{min}", 1000, 0.3, 1.3);
+	TH1D *h_t_dip = new TH1D("", ";t_{dip}", 1000, 0.3, 1.3);
+	TH1D *h_t_bmp = new TH1D("", ";t_{bmp}", 1000, 0.3, 1.3);
+	TH1D *h_t_max = new TH1D("", ";t_{max}", 1000, 0.3, 1.3);
+
+	for (unsigned int ir = 0; ir < n_repetitions; ++ir)
+	{
+		// generate biased dataset
+		Dataset ds_mod(ds);
+
+		// generate bias in t-values
+		TVectorD delta(ds_mod.m_t_value_cov.GetNrows()), rdm(ds_mod.m_t_value_cov.GetNrows());
+		for (int i = 0; i < rdm.GetNrows(); i++)
+			rdm(i) = gRandom->Gaus();
+		delta = m_gen * rdm;
+
+		ds_mod.t_min += delta(0);
+		ds_mod.t_dip += delta(1);
+		ds_mod.t_bmp += delta(2);
+		ds_mod.t_max += delta(3);
+
+		h_t_min->Fill(ds_mod.t_min);
+		h_t_dip->Fill(ds_mod.t_dip);
+		h_t_bmp->Fill(ds_mod.t_bmp);
+		h_t_max->Fill(ds_mod.t_max);
+
+		// (re)-build fit function
+		BuildFitFunction(fitModel, ds_mod);
+
+		// set extrapolated parameters to fit function
+		for (int i = 0; i < n_ff_parameters; ++i)
+			ds_mod.ff->SetParameter(i, p(i));
+
+		// sample biased extrapolation
+		for (int i = 0; i < g_ref->GetN(); ++i)
+		{
+			const double t = g_ref->GetX()[i];
+			const double f_dsdt_mod = ds_mod.ff->Eval(t);
+			const double f_B_mod = (log(ds_mod.ff->Eval(t + ep)) - log(f_dsdt_mod)) / ep;
+
+			stat[i].Fill(f_dsdt_mod);
+			stat_B[i].Fill(f_B_mod);
+		}
+
+		// clean up
+		delete ds_mod.ff;
+	}
+
+	h_t_min->Write("h_t_min");
+	h_t_dip->Write("h_t_dip");
+	h_t_bmp->Write("h_t_bmp");
+	h_t_max->Write("h_t_max");
+
+	// build graphs
+	for (int i = 0; i < g_ref->GetN(); ++i)
+	{
+		const double t = g_ref->GetX()[i];
+
+		const double f_dsdt_unc = stat[i].GetStdDev(0);
+		g_dsdt_ext_unc->SetPoint(i, t, f_dsdt_unc);
+
+		const double f_B_unc = stat_B[i].GetStdDev(0);
+		g_B_ext_unc->SetPoint(i, t, f_B_unc);
+	}
+}
+
+//----------------------------------------------------------------------------------------------------
+
+void SaveExtrapolation(const ROOT::Fit::FitResult &result, const string &extrapolationModel, const Dataset &ds, bool calculateTUnc = false)
+{
+	// calculate extrapolated parameters
+	const double *par = result.GetParams();
+
+	TVectorD a(n_ff_parameters), b(n_ff_parameters);
+	for (int i = 0; i < n_ff_parameters; ++i)
+	{
+		a(i) = par[i];
+		b(i) = par[n_ff_parameters + i];
+	}
+
+	TVectorD p = EvaluateParameters(a, b, ds.sqrt_s, extrapolationModel);
+
+	// set extrapolated parameters to fit function
+	for (int i = 0; i < n_ff_parameters; ++i)
+		ds.ff->SetParameter(i, p(i));
+
+	// evaluate extrapolated graph
+	map<string, TGraph *> m_g_ext;
+	m_g_ext["dsdt"] = new TGraph(); m_g_ext["dsdt"]->SetName("g_dsdt_ext");
+	m_g_ext["B"] = new TGraph(); m_g_ext["B"]->SetName("g_B_ext");
+
+	TGraph *g_dsdt_ext = new TGraph(); g_dsdt_ext->SetName("g_dsdt_ext");
+	TGraph *g_B_ext = new TGraph(); g_B_ext->SetName("g_B_ext");
+
+	const double ep = 1E-4;
+
+	for (unsigned int i = 0; i < n_dsdt_samples; ++i)
+	{
+		const double t = ds.t_min + (ds.t_max - ds.t_min) / (n_dsdt_samples - 1) * i;
+		const double f_dsdt = ds.ff->Eval(t);
+		const double f_B = (log(ds.ff->Eval(t + ep)) - log(f_dsdt)) / ep;
+
+		int idx = m_g_ext["dsdt"]->GetN();
+		m_g_ext["dsdt"]->SetPoint(idx, t, f_dsdt);
+		m_g_ext["B"]->SetPoint(idx, t, f_B);
+	}
+
+	for (const auto &p : m_g_ext)
+		p.second->Write();
+
+	// evaluate uncertainty bands
+	map<string, TGraph *> m_g_ext_unc_p;
+	m_g_ext_unc_p["dsdt"] = new TGraph();
+	m_g_ext_unc_p["B"] = new TGraph();
+	SaveParameterUncertaintyBand(result, extrapolationModel, ds, par, ep, m_g_ext["dsdt"], m_g_ext_unc_p["dsdt"], m_g_ext_unc_p["B"]);
+
+	map<string, TGraph *> m_g_ext_unc_t;
+	m_g_ext_unc_t["dsdt"] = new TGraph();
+	m_g_ext_unc_t["B"] = new TGraph();
+	if (calculateTUnc)
+		SaveTValueUncertaintyBand(result, extrapolationModel, ds, par, ep, m_g_ext["dsdt"], m_g_ext_unc_t["dsdt"], m_g_ext_unc_t["B"]);
+
+	// make plots
+	TDirectory *d_top = gDirectory;
+
+	for (const string &uncType : { "p", "t", "c" })
+	{
+		gDirectory = d_top->mkdir(("unc " + uncType).c_str());
+
+		for (const string &quantity : { "dsdt", "B" } )
+		{
+			const TGraph *g_ref = m_g_ext[quantity];
+
+			TGraph *g_unc = new TGraph(); g_unc->SetName(("g_"+quantity+"_ext_unc").c_str());
+			TGraph *g_pl_unc = new TGraph(); g_pl_unc->SetName(("g_"+quantity+"_ext_pl_unc").c_str());
+			TGraph *g_mi_unc = new TGraph(); g_mi_unc->SetName(("g_"+quantity+"_ext_mi_unc").c_str());
+
+			for (int i = 0; i < g_ref->GetN(); ++i)
+			{
+				const double t = g_ref->GetX()[i];
+				const double v_ref = g_ref->GetY()[i];
+
+				const double unc_p = m_g_ext_unc_p[quantity]->GetY()[i];
+				const double unc_t = (calculateTUnc) ? m_g_ext_unc_t[quantity]->GetY()[i] : 0.;
+
+				double unc = 0.;
+				if (uncType == "p") unc = unc_p;
+				if (uncType == "t") unc = unc_t;
+				if (uncType == "c") unc = sqrt(unc_p*unc_p + unc_t*unc_t);
+
+				g_unc->SetPoint(i, t, unc);
+				g_pl_unc->SetPoint(i, t, v_ref + unc);
+				g_mi_unc->SetPoint(i, t, v_ref - unc);
+			}
+
+			g_unc->Write();
+			g_pl_unc->Write();
+			g_mi_unc->Write();
+		}
+	}
+
+	gDirectory = d_top;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -389,7 +565,6 @@ int main(int argc, const char **argv)
 {
 	// defaults
 	string tRange = "";
-	string fitModel = "";
 
 	// parse command line
 	for (int argi = 1; (argi < argc) && (cl_error == 0); ++argi)
@@ -402,7 +577,7 @@ int main(int argc, const char **argv)
 
 		if (TestStringParameter(argc, argv, argi, "-range", tRange)) continue;
 		if (TestStringParameter(argc, argv, argi, "-model", fitModel)) continue;
-		
+
 		printf("ERROR: unknown option '%s'.\n", argv[argi]);
 		cl_error = 1;
 	}
@@ -459,6 +634,11 @@ int main(int argc, const char **argv)
 
 		for (bool useCorrelationMatrix : { false, true })
 		{
+			printf("--------------------------------------------------\n");
+			printf("ext model = %s, use correlation matrix = %i\n", extrapolationModel.c_str(), useCorrelationMatrix);
+			printf("--------------------------------------------------\n");
+
+
 			TDirectory *d_ucm = d_ext_mod->mkdir( (useCorrelationMatrix) ? "corr" : "uncorr");
 			gDirectory = d_ucm;
 
@@ -468,7 +648,7 @@ int main(int argc, const char **argv)
 			S2_FCN s2Fcn;
 			s2Fcn.extrapolationModel = extrapolationModel;
 			s2Fcn.useCorrelationMatrix = useCorrelationMatrix;
-			
+
 			double pStart[2*n_ff_parameters];
   			fitter.SetFCN(2*n_ff_parameters, s2Fcn, pStart, 0, true);
 
@@ -491,7 +671,7 @@ int main(int argc, const char **argv)
 
 			SaveFitPlots(result, extrapolationModel);
 
-			SaveExtrapolation(result, extrapolationModel, ds_ext);
+			SaveExtrapolation(result, extrapolationModel, ds_ext, true);
 
 			for (const auto &ds : datasets)
 			{
